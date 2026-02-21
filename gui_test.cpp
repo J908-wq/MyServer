7e754420b1f968f71f0ffb0a729a2a5f3e0ff9f6
J908-wq <orangutanorangutan93@gmail.com>
2026-02-20 21:13:14 -0800

    Add initial GUI test using ImGui and GLFW

---
+++ gui_test.cpp
@@ -0,0 +1,52 @@
+#include "external/imgui/imgui.h"
+#include "external/imgui/backends/imgui_impl_glfw.h"
+#include "external/imgui/backends/imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) return 1;
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "My GUI", nullptr, nullptr);
+    if (!window) return 1;
+    
+    glfwMakeContextCurrent(window);
+    
+    // Initialize ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+    
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        
+        ImGui::Begin("Hello GUI");
+        ImGui::Text("This is your GUI!");
+        ImGui::End();
+        
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+    
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
+    return 0;
+}
